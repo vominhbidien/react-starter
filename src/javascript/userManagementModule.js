92117fe814c3b3ca4effa543e31a54c5e91a2ad0
Hoang Vo <you@example.com>
2022-10-04 08:45:30 +0700

    reverse

---
+++ src/javascript/userManagementModule.js
@@ -0,0 +1,18 @@
+export let users = [];
+
+export function createUser(firstName, lastName, age, workAt) { }
+
+export function findAllUsers() { }
+export function findUserById(id) { }
+export function findUsers(params) { }
+
+export function updateUserById() { }
+export function updateUsers(searchParams, payload) { }
+
+export function deleteUserById(id) { }
+export function deleteUsers(searchParams) { }
+
+// For unit tests only
+export function reset() {
+  users = [];
+}
